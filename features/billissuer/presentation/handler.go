27b435a5c3b2383f9335f70bcfba1de0a749db7a
xxrunexx <rasyid.id3@gmail.com>
2021-12-30 16:27:09 +0700

    Add handler

--- features/billissuer/presentation/handler.go
+++ features/billissuer/presentation/handler.go
@@ -1,6 +1,12 @@
 package presentation
 
-import "invoice-api/features/billissuer"
+import (
+	"invoice-api/features/billissuer"
+	"invoice-api/features/billissuer/presentation/request"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type BillIssuerHandler struct {
 	billissuerBusiness billissuer.Business
@@ -8,4 +14,25 @@ type BillIssuerHandler struct {
 
 func NewHandlerBillIssuer(billissuerBusiness billissuer.Business) *BillIssuerHandler {
 	return &BillIssuerHandler{billissuerBusiness}
+}
+
+func (biHandler *BillIssuerHandler) CreateBillIssuerHandler(e echo.Context) (err error) {
+	newBillIssuer := request.ReqBillIssuer{}
+
+	if err := e.Bind(&newBillIssuer); err != nil {
+		return e.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": err.Error(),
+		})
+	}
+
+	if err := biHandler.billissuerBusiness.CreateAccount(newBillIssuer.ToBillIssuerCore()); err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": err.Error(),
+		})
+	}
+
+	return e.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Success",
+		"data":    newBillIssuer,
+	})
 }
