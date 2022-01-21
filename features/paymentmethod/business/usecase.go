b0f67666460c86764c8a186bc2632083bf8ad4e5
xxrunexx <rasyid.id3@gmail.com>
2022-01-21 07:22:44 +0700

    Add entity and business layer

--- features/paymentmethod/business/usecase.go
+++ features/paymentmethod/business/usecase.go
@@ -34,3 +34,12 @@ func (pmBusiness *PaymentMethodBusiness) GetPaymentMethodById(id int) (paymentme
 	}
 	return pmData, nil
 }
+
+func (pmBusiness *PaymentMethodBusiness) GetAllPaymentMethod(data paymentmethod.PaymentMethodCore) ([]paymentmethod.PaymentMethodCore, error) {
+	pmDatas, err := pmBusiness.paymentmethodData.GetAllPaymentMethod(data)
+
+	if err != nil {
+		return nil, err
+	}
+	return pmDatas, nil
+}
