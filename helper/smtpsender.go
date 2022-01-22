a06d6bee1a1816bfdc389447f07cfea762ba3007
xxrunexx <rasyid.id3@gmail.com>
2022-01-22 22:33:14 +0700

    Add condition for different payment status

--- helper/smtpsender.go
+++ helper/smtpsender.go
@@ -18,26 +18,41 @@ func SendGmail(inData invoice.InvoiceCore) {
 	emailTo := []string{inData.ClientEmail}
 	fmt.Println("isi emailto :", emailTo)
 	data := struct {
-		ReceiverName string
-		SenderName   string
-		Total        int
-		CreatedAt    time.Time
-		PaymentTerms int
-		PaymentDue   time.Time
+		ReceiverName  string
+		SenderName    string
+		Total         int
+		CreatedAt     time.Time
+		PaymentTerms  int
+		PaymentDue    time.Time
+		PaymentStatus string
 	}{
-		ReceiverName: inData.ClientName,
-		SenderName:   "Invoicein",
-		Total:        inData.Total,
-		CreatedAt:    inData.CreatedAt,
-		PaymentTerms: inData.PaymentTerms,
-		PaymentDue:   inData.PaymentDue,
+		ReceiverName:  inData.ClientName,
+		SenderName:    "Invoicein",
+		Total:         inData.Total,
+		CreatedAt:     inData.CreatedAt,
+		PaymentTerms:  inData.PaymentTerms,
+		PaymentDue:    inData.PaymentDue,
+		PaymentStatus: inData.PaymentStatus,
 	}
 
-	status, err := SendEmailSMTP(emailTo, data, "sample_template.html", inData)
-	if err != nil {
-		log.Println(err)
+	if data.PaymentStatus == "paid" {
+		status, err := SendEmailSMTP(emailTo, data, "success_invoice.html", inData)
+
+		if err != nil {
+			log.Println(err)
+		}
+		if status {
+			log.Println("Email sent successfully using SMTP")
+		}
+	} else if data.PaymentStatus == "processed" {
+		status, err := SendEmailSMTP(emailTo, data, "new_invoice.html", inData)
+
+		if err != nil {
+			log.Println(err)
+		}
+		if status {
+			log.Println("Email sent successfully using SMTP")
+		}
 	}
-	if status {
-		log.Println("Email sent successfully using SMTP")
-	}
+
 }
